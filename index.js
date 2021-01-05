8bef5a1bdb414680d2de6fb0a51608c0c4dfe1de
dws-nishoo <nishoo.gupta@rankwatch.com>
2021-01-05 13:29:30 +0530

    saved

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+const port = process.env.PORT || 3000;
 const User = require('./routes/User');
 const Business = require('./routes/Business');
 const Product = require('./routes/Product');
 app.use('/user', User);
 app.use('/business', Business);
 app.use('/product', Product);
-app.listen('3000', 'localhost', ()=>{
+app.listen(port, 'localhost', ()=>{
 	console.log('application is running on 3000');
 });
